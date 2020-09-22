ba6ff81dcb9615fd4fde268e03fc5cec930d17c8
AHMED AFANDY <eng.computer1@yahoo.com>
2020-09-22 01:58:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // Your code here
 
-class Polygon{
-constructor(countSides){
-console.log(countSides)
-  this._countside=countSides;
+class Polygon {
+
+    constructor(countSides){
+      this._countSides = countSides;
+    }
+
+    get countSides(){
+      return this._countSides = (this._countSides).length
+    }
+
+    get perimeter() {
+      return this._countSides = this._countSides.reduce((acc,element) =>acc+element)
+    }
+
 }
 
-get countSides(){
 
-  return this._countSides = this._countSides.size()
 
-}
+class Triangle  extends Polygon {
+
+    get isValid()
+    {
+      return  this. _countSides == 3 ? true:false
+    }
+
+  }
+
+
+
+  class Square  extends Polygon {
+
+    get isValid()
+    {
+      return  this. _countSides == 4 ? true:false
+    }
+
+
+    }
